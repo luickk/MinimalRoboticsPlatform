2186568c27348288fb6bdd9d6f66ad241b55056a
Luick Klippel <luickklippel@Luicks-MacBook-Pro.fritz.box>
2022-07-30 12:38:30 +0200

    first booting version

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,16 @@
 #![no_std]
 #![no_main]
 
-#[panic_handler]
-fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
-    loop {}
-}
-
-const MMIO_SERIAL: *mut u64 = 0x09000000 as *mut u64;
-
+mod serial;
 
 core::arch::global_asm!(include_str!("boot.S"));
 
-fn put_char(ch: u8) {
-	unsafe {
-		*MMIO_SERIAL = ch as u64;	
-	} 
-}
-
-pub fn kprint(inp: &str) {
-	let mut i: usize = 0;
-	let inp_bytes = inp.as_bytes();
-    while i <= inp.len() {
-        put_char(inp_bytes[i]);
-        i += 1;
-    }
-}
-
 #[no_mangle]
 pub extern "C" fn kernel_main() {
-	kprint("test \n");
+	serial::kprint("test \n");
+}
+
+#[panic_handler]
+fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
 }
